86c6cf3ad01233f1ef6d131eeb815cf9c6fb2540
LondonTorres <londonztorres@gmail.com>
2020-12-07 13:39:10 -0500

    Combined testing and special cases into one utility. Implemented saving. Revised progress meter.

---
+++ generator.h
@@ -0,0 +1,53 @@
+#include <vector>
+
+using namespace std;
+
+//stores ints as a vector and an array, also the max value if needed
+
+class Gen {
+public:
+    vector<int> vec;
+    int* arr;
+    int size;
+    int max;
+    Gen(int size);
+    void Randomize();
+    void Ascending();
+    void Descending();
+};
+
+//parameter: number of elements in array
+Gen::Gen(int size) {
+    this->size = size;
+    arr = new int[size];
+}
+
+void Gen::Randomize()
+{
+    vec.clear();
+    srand(time(0));
+    int r;
+    for (int i = 0; i < size; i++) {
+        r = rand() % INT_MAX;
+        arr[i] = r;
+        vec.push_back(r);
+    }
+}
+
+void Gen::Ascending()
+{
+    vec.clear();
+    for (int i = 0; i < size; i++) {
+        arr[i] = i;
+        vec.push_back(i);
+    }
+}
+
+void Gen::Descending()
+{
+    vec.clear();
+    for (int i = 0; i < size; i++) {
+        arr[i] = size - 1 - i;
+        vec.push_back(size - 1 - i);
+    }
+}
--- main.cpp
+++ main.cpp
@@ -5,14 +5,12 @@
 #include <chrono>
 #include "matplotlibcpp.h"
 #include <climits>
-#include "randomizer.h"
+#include "generator.h"
 #include "sorting.h"
 
 using namespace std;
 namespace plt = matplotlibcpp;
 
-void CompareAlgorithms();
-void SpecialCases();
 void callSort(int algorithm, vector<int>& values);
 
 int main()
@@ -20,144 +18,162 @@ int main()
     while(true)
     {
         string input;
-        cout << "Main Menu. Select an option." << endl;
-        cout << "1. Compare Algorithms" << endl;
-        cout << "2. Special Cases" << endl;
-        cout << "3. End" << endl;
+
+        cout << "Which sorting algorithm would you like to test?" << endl;
+        cout << " 1. Selection Sort" << endl;
+        cout << " 2. Bubble Sort" << endl;
+        cout << " 3. Insertion Sort" << endl;
+        cout << " 4. Shell Sort (gap = 2.2)" << endl;
+        cout << " 5. Merge Sort" << endl;
+        cout << " 6. Quick Sort" << endl;
+        cout << " 7. Heap Sort" << endl;
+        cout << " 8. Radix Sort" << endl;
+        cout << " 9. Bucket Sort" << endl;
+        cout << "10. Counting Sort" << endl;
+        cout << "11. Pancake Sort" << endl;
+        cout << "12. Exit the utility" << endl;
+
         cin >> input;
-        if(input[0] == '3')
+        int algorithmSelection = stoi(input);
+        if(algorithmSelection == 12)
             break;
-        if(input[0] == '1')
+        if(algorithmSelection > 11 || algorithmSelection < 1)
         {
-            CompareAlgorithms();
+            cout << "Invalid input." << endl;
+            continue;
         }
-        else if(input[0] == '2')
+
+        cout << "What type of array should be tested?" << endl;
+        cout << "1. Randomized Array" << endl;
+        cout << "2. Ascending Order (Sorted Order)" << endl;
+        cout << "3. Descending Order (Reverse Order)" << endl;
+        cin >> input;
+        int arrayType = stoi(input);
+        if(arrayType > 3 || arrayType < 1)
         {
-            SpecialCases();
+            cout << "Invalid input." << endl;
+            continue;
         }
-        else
-            cout << "Invalid input. Please type a number from 1 to 3." << endl;
+
+        vector<int> arrayLengths;
+        cout << "What range of array sizes should be tested? Enter a lower bound, upper bound, and interval separated by spaces. Example: 10 1000 10" << endl;
+        cin >> input;
+        int lower = stoi(input);
+        cin >> input;
+        int upper = stoi(input);
+        cin >> input;
+        int interval = stoi(input);
+
+        cout << "How many trials should be done for each size?" << endl;
+        cin >> input;
+        int trials = stoi(input);
+
+
+        vector<int> sizes;
+        vector<long> times;
+        long double total = 0;
+        for(int arrayLength = lower; arrayLength <= upper; arrayLength += interval)
+            for(int i = 0; i < trials; i++)
+                total += (arrayLength/1e120)*(arrayLength/1e120);
+
+        long double count = 0;
+        int yMax = INT_MIN;
+        cout.flush();
+        for(int arrayLength = lower; arrayLength <= upper; arrayLength += interval)
+        {
+            for(int i = 0; i < trials; i++)
+            {
+                Gen values(arrayLength);
+                if(arrayType == 1)
+                    values.Randomize();
+                else if(arrayType == 2)
+                    values.Ascending();
+                else if(arrayType == 3)
+                    values.Descending();
+                auto start = chrono::high_resolution_clock::now();
+                callSort(algorithmSelection, values.vec);
+                auto end = chrono::high_resolution_clock::now();
+                auto executionTime = chrono::duration_cast<chrono::microseconds>(end - start);
+                sizes.push_back(arrayLength);
+                times.push_back(executionTime.count());
+                if(executionTime.count() > yMax)
+                    yMax = executionTime.count();
+                count += (arrayLength/1e120)*(arrayLength/1e120);
+                cout << "[" << count / total * 100 << "%]\r";
+                cout.flush();
+            }
+        }
+        cout << endl;
+
+        string algorithmName;
+        switch(algorithmSelection)
+        {
+            case 1:
+                algorithmName = "Selection Sort";
+                break;
+            case 2:
+                algorithmName = "Bubble Sort";
+                break;
+            case 3:
+                algorithmName = "Insertion Sort";
+                break;
+            case 4:
+                algorithmName = "Shell Sort";
+                break;
+            case 5:
+                algorithmName = "Merge Sort";
+                break;
+            case 6:
+                algorithmName = "Quick Sort";
+                break;
+            case 7:
+                algorithmName = "Heap Sort";
+                break;
+            case 8:
+                algorithmName = "Radix Sort";
+                break;
+            case 9:
+                algorithmName = "Bucket Sort";
+                break;
+            case 10:
+                algorithmName = "Counting Sort";
+                break;
+            case 11:
+                algorithmName = "Pancake Sort";
+        }
+
+        string testType;
+        switch(arrayType)
+        {
+            case 1:
+                testType = "Arrays Randomized";
+                break;
+            case 2:
+                testType = "Arrays in Sorted Order";
+                break;
+            case 3:
+                testType = "Arrays in Reverse Sorted Order";
+        }
+
+        plt::figure_size(1200, 780);
+        plt::scatter(sizes, times);
+        plt::title("Execution time of " + algorithmName + ", " + testType);
+        plt::xlim(0,upper);
+        plt::ylim(0, yMax);
+        plt::xlabel("Array Size, n");
+        plt::ylabel("Execution Time (Microseconds)");
+        cout << "Would you like to save this plot? (Y/N)" << endl;
+        cin >> input;
+        if(input[0] == 'Y')
+        {
+            cout << "Enter a file name. Do not include a file extension." << endl;
+            cin >> input;
+            plt::save(input + ".png");
+        }
+        plt::show();
+        cout << endl;
     }
     return 0;
-}
-
-void CompareAlgorithms()
-{
-    string input;
-    vector<int> arrayLengths;
-    cout << "What range of array sizes should be tested? Enter a lower bound, upper bound, and interval separated by spaces. Example: 10 1000 10" << endl;
-    cin >> input;
-    int lower = stoi(input);
-    cin >> input;
-    int upper = stoi(input);
-    cin >> input;
-    int interval = stoi(input);
-
-    cout << "How many trials should be done for each size?" << endl;
-    cin >> input;
-    int trials = stoi(input);
-
-    cout << "Which sorting algorithm would you like to test?" << endl;
-    cout << "1.  Selection Sort" << endl;
-    cout << "2.  Bubble Sort" << endl;
-    cout << "3.  Insertion Sort" << endl;
-    cout << "4.  Shell Sort (gap = 2.2)" << endl;
-    cout << "5.  Merge Sort" << endl;
-    cout << "6.  Quick Sort" << endl;
-    cout << "7.  Heap Sort" << endl;
-    cout << "8.  Radix Sort" << endl;
-    cout << "9.  Bucket Sort" << endl;
-    cout << "10. Counting Sort" << endl;
-    cout << "11. Pancake Sort" << endl;
-
-    cin >> input;
-    int algorithmSelection = stoi(input);
-    if(algorithmSelection > 11 || algorithmSelection < 1)
-    {
-        cout << "Invalid input." << endl;
-        return;
-    }
-
-    vector<int> sizes;
-    vector<long> times;
-    int totalTrials = trials * ((upper - lower) / interval + 1);
-    int count = 0;
-    int yMax = INT_MIN;
-    cout << "[0/" << arrayLengths.size() * trials << "]\r";
-    cout.flush();
-    for(int arrayLength = lower; arrayLength <= upper; arrayLength += interval)
-    {
-        for(int i = 0; i < trials; i++)
-        {
-            RNG values(arrayLength);
-            auto start = chrono::high_resolution_clock::now();
-            callSort(algorithmSelection, values.vec);
-            auto end = chrono::high_resolution_clock::now();
-            auto executionTime = chrono::duration_cast<chrono::microseconds>(end - start);
-            sizes.push_back(arrayLength);
-            times.push_back(executionTime.count());
-            if(executionTime.count() > yMax)
-                yMax = executionTime.count();
-            count++;
-            cout << "[" << count << "/" << totalTrials << "]\r";
-            cout.flush();
-        }
-    }
-    cout << endl;
-
-    string algorithmName;
-    switch(algorithmSelection)
-    {
-        case 1:
-            algorithmName = "Selection Sort";
-            break;
-        case 2:
-            algorithmName = "Bubble Sort";
-            break;
-        case 3:
-            algorithmName = "Insertion Sort";
-            break;
-        case 4:
-            algorithmName = "Shell Sort";
-            break;
-        case 5:
-            algorithmName = "Merge Sort";
-            break;
-        case 6:
-            algorithmName = "Quick Sort";
-            break;
-        case 7:
-            algorithmName = "Heap Sort";
-            break;
-        case 8:
-            algorithmName = "Radix Sort";
-            break;
-        case 9:
-            algorithmName = "Bucket Sort";
-            break;
-        case 10:
-            algorithmName = "Counting Sort";
-            break;
-        case 11:
-            algorithmName = "Pancake Sort";
-    }
-
-    plt::figure_size(1200, 780);
-    plt::scatter(sizes, times);
-    plt::title("Execution time of " + algorithmName);
-    plt::xlim(0,upper);
-    plt::ylim(0, yMax);
-    plt::xlabel("Array Size, n");
-    plt::ylabel("Execution Time (Microseconds)");
-    plt::show();
-
-    cout << "Would you like to save this plot?" << endl;
-
-}
-
-void SpecialCases()
-{
-
 }
 
 void callSort(int algorithm, vector<int>& values)
